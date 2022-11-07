6c752df5e70a33f32918a430386e83a3709d2809
Lazola2 <lazolamakubalo271@gmail.com>
2022-11-07 14:19:33 +0200

    variable exercise

---
+++ variableExercise/JS/code.js
@@ -0,0 +1,11 @@
+/**
+ * create 3 variable string, number, boolean
+ * display them on the document(not console)
+ */
+
+let userName = "Reece"
+let userAge = 30
+let isSouthAfricanCitizen = true
+
+let output = `Username: ${userName} <br>Age: ${userAge} <br>From SA: ${isSouthAfricanCitizen}`
+document.write(output)
